6cb8844510ab1e4c595ae2639ef2dabe562de8ee
voxipropiedades <voxi.2026b@gmail.com>
2026-04-16 17:01:18 -0600

    Update SW to v2 for cache busting

--- sw.js
+++ sw.js
@@ -1,16 +1,30 @@
+const CACHE_NAME = 'voxi-hr-v2';
+const ASSETS = [
+  '/Mis-propiedades-/',
+  '/Mis-propiedades-/index.html',
+  '/Mis-propiedades-/manifest.json',
+  '/Mis-propiedades-/icon.png'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('voxi-hr-v1').then((cache) => cache.addAll([
-      '/Mis-propiedades-/',
-      '/Mis-propiedades-/index.html',
-      '/Mis-propiedades-/manifest.json',
-      '/Mis-propiedades-/logo.jpg'
-    ])),
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
