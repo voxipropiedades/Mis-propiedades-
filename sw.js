5804d80858bd9a42fccdd1b2dbee7981d61cbf45
voxipropiedades <voxi.2026b@gmail.com>
2026-04-24 19:15:33 -0600

    Update cache version to v3

--- sw.js
+++ sw.js
@@ -1,1 +1,33 @@
-self.addEventListener('install', e => self.skipWaiting()); self.addEventListener('activate', e => self.registration.unregister().then(() => self.clients.matchAll()).then(c => c.forEach(cl => cl.navigate(cl.url))));
+const CACHE_NAME = 'voxi-cache-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return Promise.allSettled(urlsToCache.map(url => cache.add(url)));
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
