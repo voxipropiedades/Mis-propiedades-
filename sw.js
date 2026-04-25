657eef80b5fb8403df3d64c2850ca1a821ec9683
voxipropiedades <voxi.2026b@gmail.com>
2026-04-24 18:01:31 -0600

    Fix Service Worker for mobile connectivity

--- sw.js
+++ sw.js
@@ -1,21 +1,33 @@
-const CACHE_NAME = 'voxi-cache-v1';
+const CACHE_NAME = 'voxi-cache-v2';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json',
-  './icon-192.png'
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return Promise.allSettled(urlsToCache.map(url => cache.add(url)));
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
