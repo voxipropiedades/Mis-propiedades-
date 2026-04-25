cd11f27aa9e67f2c8f67497b312f815ce69329f9
voxipropiedades <voxi.2026b@gmail.com>
2026-04-24 20:11:52 -0600

    FORCE RESET: Limpieza profunda de caché y Service Worker

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-self.addEventListener('install', e => self.skipWaiting()); self.addEventListener('activate', e => self.registration.unregister().then(() => self.clients.matchAll()).then(c => c.forEach(cl => cl.navigate(cl.url))));
+self.addEventListener('install', e => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => caches.delete(key)));
+        }).then(() => {
+            return self.registration.unregister();
+        }).then(() => {
+            return self.clients.matchAll();
+        }).then(clients => {
+            clients.forEach(client => client.navigate(client.url));
+        })
+    );
+});
