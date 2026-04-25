7b0f866d80d4f876a53c850b0a4b75098723d1e4
voxipropiedades <voxi.2026b@gmail.com>
2026-04-24 20:15:20 -0600

    EMERGENCY RESTORE: Eliminando kill-switch y restaurando acceso directo

--- sw.js
+++ sw.js
@@ -1,17 +1,7 @@
-self.addEventListener('install', e => {
-    self.skipWaiting();
-});
-
+self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys.map(key => caches.delete(key)));
-        }).then(() => {
-            return self.registration.unregister();
-        }).then(() => {
-            return self.clients.matchAll();
-        }).then(clients => {
-            clients.forEach(client => client.navigate(client.url));
-        })
-    );
+    e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(fetch(e.request));
 });
