dbb1986f22dac273ce1ef31f4a30cc58eb20c8ef
voxipropiedades <voxi.2026b@gmail.com>
2026-04-26 08:41:08 -0600

    SW v47: Forzar actualización

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => {
-    e.waitUntil(self.clients.claim());
+// sw.js V47 - NUCLEAR RESET
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-    e.respondWith(fetch(e.request));
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => caches.delete(key))
+        )).then(() => self.clients.claim())
+    );
+});
+self.addEventListener('fetch', (e) => {
+    // Bypass SW entirely
+    return; 
 });
